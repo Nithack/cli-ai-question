13c1ff9cf1eab15a3dbe25f761092185188b6121
nithack <andreynithack@gmail.com>
2023-05-03 02:49:17 -0300

    feat: add 'ask' command to CLI tool to interact with OpenAI API feat: add dotenv package to load environment variables from .env file feat: add openai package to interact with OpenAI API refactor: change file mode of index.js to 100755 to make it executable

--- src/index.js
+++ src/index.js
@@ -1,1 +1,42 @@
-console.log("Hello World!")
+#!/usr/bin/env node
+
+require('dotenv').config();
+const program = require('commander');
+const package = require('./../package.json');
+const { Configuration, OpenAIApi } = require("openai");
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+
+const openai = new OpenAIApi(configuration);
+
+program.version(package.version);
+
+
+
+async function ask(question) {
+  const basicQuestion = [
+    { "role": "user", "content": question },
+  ]
+  await openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    messages: basicQuestion,
+  }).then((response) => {
+    console.log(response.data.choices[0].message.content)
+    return response.data.choices[0].message.content;
+  }).catch((err) => {
+    console.log(err.response);
+  });
+}
+
+program
+  .command('ask [pergunta]')
+  .description('Faz uma pergunta')
+  .action(async (todo) => {
+    console.log(await ask(todo));
+  });
+
+
+program.parse(process.argv);
