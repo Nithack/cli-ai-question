20e4198d11ef1fb05b72c47e0b2d60d3e3cefd87
nithack <andreynithack@gmail.com>
2023-05-09 00:22:07 -0300

    chore: remove babel.config.cjs file test(package.json): add NODE_OPTIONS flag to jest script to enable experimental-vm-modules and add test:cov script to generate coverage report feat(package.json): add coveragePathIgnorePatterns to jest configuration to ignore .interface.js files from coverage report feat(OpenAI.js): add OpenAI API key to constructor and createChatCompletion method to get answer from OpenAI API test(opena-ai.spec.js): add tests to OpenAI class, including tests for error handling and response from OpenAI API

--- src/infra/OpenAI/OpenAI.js
+++ src/infra/OpenAI/OpenAI.js
@@ -1,11 +1,27 @@
 import OpenAIInterface from './OpenAi.interface.js';
+import { OpenAIApi, Configuration } from 'openai';
 class OpenAI extends OpenAIInterface {
 
-  constructor(name) {
+  constructor() {
     super()
+    this.openai = new OpenAIApi(new Configuration({
+      apiKey: process.env.OPENAI_API_KEY,
+    }))
   }
   async getAnswer(question) {
-    return 0;
+
+    return await this.openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: [
+        { "role": "user", "content": question },
+      ],
+    }).then((response) => {
+      console.log('then', response)
+      return response.data.choices[0].message.content
+    }).catch((err) => {
+      throw new Error(`[ERROR] ${err.response.data.error.message} [${err.response.status}]`);
+    });
+
   }
 }
 
--- test/infra/OpenAI/opena-ai.spec.js
+++ test/infra/OpenAI/opena-ai.spec.js
@@ -1,15 +1,40 @@
-import OpenAI from '../../../src/infra/OpenAI/OpenAI';
+import OpenAI from '../../../src/infra/OpenAI/OpenAI'
+import { describe, expect, jest } from '@jest/globals'
+
+// mock OPENAI MODULE
 describe('OpenAI', () => {
-  let openAI;
+  let openAI
   beforeEach(() => {
-    openAI = new OpenAI('teste');
+    openAI = new OpenAI()
+    jest.clearAllMocks()
+  })
 
-  });
-
-  test('should return a response question', async () => {
+  test('should openai return status 401 because api key is not undefined', async () => {
+    const defaultErrorAPIKEY = `Incorrect API key provided: undefined. You can find your API key at https://platform.openai.com/account/api-keys.`
+    jest.spyOn(openAI.openai, 'createChatCompletion').mockRejectedValueOnce({
+      response: {
+        data: {
+          error: {
+            message: defaultErrorAPIKEY
+          }
+        },
+        status: 401
+      }
+    })
     const answer = 'What this is app use for?'
-    const answerResponse = 'This app is used to answer questions about anything.'
-    const response = await openAI.getAnswer(answer);
-    expect(response).toEqual(answerResponse);
-  });
-});
+    expect(openAI.getAnswer(answer)).rejects.toThrowError(`[ERROR] ${defaultErrorAPIKEY} [401]`)
+  })
+  test('should openai return response', async () => {
+    jest.spyOn(openAI.openai, 'createChatCompletion').mockResolvedValue({
+      data: {
+        choices: [{
+          message: {
+            content: 'This is a test'
+          }
+        }]
+      }
+    })
+    const answer = 'What this is app use for?'
+    expect(await openAI.getAnswer(answer)).toEqual('This is a test')
+  })
+})
